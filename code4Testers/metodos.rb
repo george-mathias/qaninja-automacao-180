9d56eda11427ff5d1a5b41f0441cb4418e40d341
George Mathias <gmathias80@gmail.com>
2021-12-17 10:49:27 -0300

    Métodos

---
+++ code4Testers/metodos.rb
@@ -0,0 +1,17 @@
+# def imprime_nome(nome)
+#     puts nome
+# end
+
+# imprime_nome("Mathias")
+
+# def somar(valor1, valor2)
+#     puts valor1 + valor2
+# end
+
+# somar(1, 2)
+
+def pega_soma(valor1, valor2)
+    valor1 + valor2
+end
+
+puts pega_soma(2, 3)
